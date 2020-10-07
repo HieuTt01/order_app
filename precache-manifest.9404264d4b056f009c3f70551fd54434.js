e50a81c99d5efb981bc16e69880557e34af5fbb2
= <=>
2020-10-07 09:27:26 +0700

    Updates

---
+++ precache-manifest.9404264d4b056f009c3f70551fd54434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e14a22d200413c8a2b90322ccdaee8",
+    "url": "/order_app/index.html"
+  },
+  {
+    "revision": "a618103f65d514905e80",
+    "url": "/order_app/static/css/main.603b6265.chunk.css"
+  },
+  {
+    "revision": "ad6637896cdcd1344967",
+    "url": "/order_app/static/js/2.618465cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/order_app/static/js/2.618465cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a618103f65d514905e80",
+    "url": "/order_app/static/js/main.23336339.chunk.js"
+  },
+  {
+    "revision": "2b26a48410e5985574fa",
+    "url": "/order_app/static/js/runtime-main.8c391a72.js"
+  }
+]);
